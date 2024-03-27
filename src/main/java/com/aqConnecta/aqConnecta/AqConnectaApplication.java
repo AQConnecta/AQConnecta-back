343c07075d0cbdd0f0b117740aabc735c9308b27
Joao Moraski <joaomoraskilunkes@gmail.com>
2024-03-27 20:23:59 -0300

    iniciando o projeto

---
+++ src/main/java/com/aqConnecta/aqConnecta/AqConnectaApplication.java
@@ -0,0 +1,13 @@
+package com.aqConnecta.aqConnecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqConnectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqConnectaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aqConnecta/aqConnecta/AqConnectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqConnecta.aqConnecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqConnectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
