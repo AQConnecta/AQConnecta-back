f5bf06ecc101d1ccd48c59c3d85443a1fbf79f0b
Kaio Felps <kaiofelipe2005@gmail.com>
2026-03-07 22:10:42 -0300

    change(`UsuarioRepository`): anota `findByUserUrl` com `@EntityGraph` para garantir eager-loading das entidades associadas & retorna um primitivo `boolean` no método `existsByUserUrl`

--- src/main/java/com/aqConnecta/repository/UsuarioRepository.java
+++ src/main/java/com/aqConnecta/repository/UsuarioRepository.java
@@ -1,28 +1,35 @@
 package com.aqConnecta.repository;
 
+import com.aqConnecta.model.Usuario;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.aqConnecta.model.Usuario;
-import org.springframework.stereotype.Repository;
-
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
-	Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 
-	Optional<Usuario> findByUserUrl(String userUrl);
+    @EntityGraph(attributePaths = {
+        "permissao",
+        "competencias",
+        "enderecos",
+        "experiencias",
+        "formacoesAcademicas",
+        "curriculo"
+    })
+    Optional<Usuario> findByUserUrl(String userUrl);
 
-	List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+    List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 
-//	List<Usuario> findAllByUserUrl(String userUrl);
+    boolean existsByUserUrl(String userUrl);
 
-	Boolean existsByUserUrl(String userUrl);
+    Usuario findByEmailIgnoreCase(String email);
 
-	Usuario findByEmailIgnoreCase(String email);
-
-	Boolean existsByEmail(String email);
+    Boolean existsByEmail(String email);
 
 }
