57313faee83960b58298da177344d7731f168c43
riumecarlos <riumecarlos@hotmail.com>
2026-09-04 16:36:16 -0300

    test: arrumando os testas para rodar a pipe

--- src/test/java/com/aqConnecta/aqConnecta/AqConnectaApplicationTests.java
+++ src/test/java/com/aqConnecta/aqConnecta/AqConnectaApplicationTests.java
@@ -1,10 +1,23 @@
 package com.aqConnecta.aqConnecta;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AqConnectaApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static final MariaDBContainer<?> databaseContainer = new MariaDBContainer<>("mariadb:10.10");
 
 	@Test
 	void contextLoads() {
